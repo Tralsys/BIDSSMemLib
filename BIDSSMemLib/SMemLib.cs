8ec886d0284a297b453585f4de4563c1a0d5d2f7
Tetsu Otter <tetsu_otter@aol.com>
2018-11-03 22:50:25 +0900

    初期セッティング

---
+++ BIDSSMemLib/SMemLib.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.MemoryMappedFiles;
+namespace TR.BIDSSMemLib
+{
+  public class SMemLib
+  {
+    public SMemLib()
+    {
+
+    }
+    MemoryMappedFile MMF = MemoryMappedFile.CreateOrOpen("BIDSSharedMemory", 0);
+  }
+}
