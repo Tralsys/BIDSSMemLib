9a7e6dc363da723a241d2e1e21d7651536a5d9d0
ＴＲ <public@technotter.com>
2019-05-04 18:15:48 +0900

    Diet and Add StandardLib

--- BIDSSMemLib/Properties/AssemblyInfo.cs
+++ BIDSSMemLib/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BIDSSMemLib")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Tetsu Otter 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
