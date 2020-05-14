f4fa153ecfa4396efc176cafb8aaf95e448493f5
ＴＲ <public@technotter.com>
2020-05-15 01:02:53 +0900

    アセンブリ設定の修正

--- BIDSSMemLib.obve/Properties/AssemblyInfo.cs
+++ BIDSSMemLib.obve/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("BIDS Shared Memory Library for openBVE")]
-[assembly: AssemblyDescription("Write the driving data to shared memory")]
+[assembly: AssemblyDescription("R/W the driving data in shared memory")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tech Otter")]
 [assembly: AssemblyProduct("BIDS Project")]
-[assembly: AssemblyCopyright("Copyright © Tetsu Otter 2019")]
+[assembly: AssemblyCopyright("Copyright © Tetsu Otter 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
