f676805a411a2b07713deb8232de81809a2d4b93
ＴＲ <public@technotter.com>
2020-05-24 21:40:20 +0900

    Panel / Soundを配列で書き込む関数の実装

--- BIDSSMemLib/SMemLib.cs
+++ BIDSSMemLib/SMemLib.cs
@@ -185,9 +185,17 @@ namespace TR.BIDSSMemLib
 		/// <param name="D">書き込む構造体</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]//関数のインライン展開を積極的にやってもらう.
 		public static void Write(in PanelD D) => SMC_PnlD?.WriteArr(D.Panels);
+		/// <summary>Panel構造体の情報を共有メモリに書き込む</summary>
+		/// <param name="D">書き込む配列</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]//関数のインライン展開を積極的にやってもらう.
+		public static void WritePanel(in int[] D) => SMC_PnlD?.WriteArr(D);
 		/// <summary>Sound構造体の情報を共有メモリに書き込む</summary>
 		/// <param name="D">書き込む構造体</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]//関数のインライン展開を積極的にやってもらう.
 		public static void Write(in SoundD D) => SMC_SndD?.WriteArr(D.Sounds);
+		/// <summary>Sound構造体の情報を共有メモリに書き込む</summary>
+		/// <param name="D">書き込む配列</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]//関数のインライン展開を積極的にやってもらう.
+		public static void WriteSound(in int[] D) => SMC_SndD?.WriteArr(D);
 	}
 }
