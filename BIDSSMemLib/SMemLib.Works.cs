6bce9912a378e7369313ae7616c86eceb853e415
ＴＲ <public@technotter.com>
2020-11-17 06:02:05 +0900

    Update SMemLib.Works.cs
    番号と意味をenumで関連付けることにより, わかりやすさ向上を狙った.

--- BIDSSMemLib/SMemLib.Works.cs
+++ BIDSSMemLib/SMemLib.Works.cs
@@ -5,31 +5,40 @@ namespace TR.BIDSSMemLib
 {
 	public static partial class SMemLib
 	{
+		public enum ARNum
+		{
+			All = 0,
+			OpenD = 1,
+			BSMD = 5,
+			PanelD = 6,
+			SoundD = 7
+		};
+
 		/// <summary>AutoReadを開始します。</summary>
 		/// <param name="ModeNum">自動読み取りを開始する情報種類</param>
 		/// <param name="Interval">読み取り頻度[ms]</param>
 		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
-		public static void ReadStart(int ModeNum = 0, int Interval = 50)
+		public static void ReadStart(in int ModeNum = 0, in int Interval = 50)
 		{
 			if (NO_SMEM_MODE) return;
 			switch (ModeNum)
 			{
-				case 1://OpenD
+				case (int)ARNum.OpenD://OpenD
 					if (SMC_OpenD?.No_SMem_Mode == false)
 						SMC_OpenD?.AR_Start(Interval);
 					else throw new InvalidOperationException("OpenD共有メモリが有効化されていません。");
 					break;
-				case 5://BSMD
+				case (int)ARNum.BSMD://BSMD
 					if (SMC_BSMD?.No_SMem_Mode == false)
 						SMC_BSMD?.AR_Start(Interval);
 					else throw new InvalidOperationException("BSMD共有メモリが有効化されていません。");
 					break;
-				case 6://PanelD
+				case (int)ARNum.PanelD://PanelD
 					if (SMC_PnlD?.No_SMem_Mode == false)
 						SMC_PnlD?.AR_Start(Interval);
 					else throw new InvalidOperationException("PanelD共有メモリが有効化されていません。");
 					break;
-				case 7://Sound D
+				case (int)ARNum.SoundD://Sound D
 					if (SMC_SndD?.No_SMem_Mode == false)
 						SMC_SndD?.AR_Start(Interval);
 					else throw new InvalidOperationException("SoundD共有メモリが有効化されていません。");
@@ -38,29 +47,39 @@ namespace TR.BIDSSMemLib
 			if (ModeNum <= 0) for (int i = 1; i < 8; i++) ReadStart(i, Interval);
 		}
 
+		/// <summary>AutoReadを開始します。</summary>
+		/// <param name="ModeNum">自動読み取りを開始する情報種類</param>
+		/// <param name="Interval">読み取り頻度[ms]</param>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static void ReadStart(in ARNum num, in int Interval = 50) => ReadStart((int)num, Interval);
 
 
 		/// <summary>AutoReadを終了します。実行中でなくともエラーは返しません。TimeOut:1000ms</summary>
 		/// <param name="ModeNum">終了させる情報種類</param>
 		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
-		public static void ReadStop(int ModeNum = 0)
+		public static void ReadStop(in int ModeNum = 0)
 		{
 			switch (ModeNum)
 			{
-				case 1://OpenD
+				case (int)ARNum.OpenD://OpenD
 					SMC_OpenD?.AR_Stop();
 					break;
-				case 5://BSMD
+				case (int)ARNum.BSMD://BSMD
 					SMC_BSMD?.AR_Stop();
 					break;
-				case 6://PanelD
+				case (int)ARNum.PanelD://PanelD
 					SMC_PnlD?.AR_Stop();
 					break;
-				case 7://Sound D
+				case (int)ARNum.SoundD://Sound D
 					SMC_SndD?.AR_Stop();
 					break;
 			}
 			if (ModeNum <= 0) for (int i = 1; i < 8; i++) ReadStop(i);
 		}
+
+		/// <summary>AutoReadを終了します。実行中でなくともエラーは返しません。TimeOut:1000ms</summary>
+		/// <param name="ModeNum">終了させる情報種類</param>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static void ReadStop(in ARNum num) => ReadStop((int)num);
 	}
 }
