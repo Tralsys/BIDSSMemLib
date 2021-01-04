75fc5a59f4ec4d09c4b744a01aea206ce1d0f109
ＴＲ <public@technotter.com>
2021-01-04 19:14:50 +0900

    公開プロパティ実装を専用ファイルに分離

--- BIDSSMemLib.rw/SMemLib.cs
+++ BIDSSMemLib.rw/SMemLib.cs
@@ -8,7 +8,6 @@ namespace TR.BIDSSMemLib
 		public static readonly string VersionNum = VersionNumInt.ToString();
 		public static readonly int VersionNumInt = 202;
 		internal const MethodImplOptions MIOpt = (MethodImplOptions)256;//MethodImplOptions.AggressiveInlining;
-		public static bool NO_SMEM_MODE { get; private set; } = false;
 
 		public const string MMFB_Name = "BIDSSharedMemory";
 		public const string MMFO_Name = "BIDSSharedMemoryO";
@@ -20,57 +19,6 @@ namespace TR.BIDSSMemLib
 		private static SMemCtrler<int> SMC_PnlD = null;
 		private static SMemCtrler<int> SMC_SndD = null;
 		//private ArrDSMemCtrler<StaD> SMC_StaD = null;
-
-		
-		/// <summary>BIDSSharedMemoryのデータ</summary>
-		public static BIDSSharedMemoryData BIDSSMemData
-		{
-			get => SMC_BSMD?.Data ?? default;
-			private set => SMC_BSMD?.Write(value);
-		}
-		/// <summary>OpenBVEでのみ得られるデータ(open専用)</summary>
-		public static OpenD OpenData
-		{
-			get => SMC_OpenD?.Data ?? default;
-			private set => SMC_OpenD?.Write(value);
-		}
-
-		///// <summary>駅情報</summary>
-		/*
-		public StaD Stations
-		{
-			get => SMC_StaD?.Data ?? default;
-			private set => SMC_StaD.Data = value;
-		}*/
-
-		/// <summary>Panel配列情報</summary>
-		[Obsolete("PanelA(int型配列)を使用してください.")]
-		public static PanelD Panels
-		{
-			get => new PanelD() { Panels = SMC_PnlD?.ArrData ?? new int[0] };
-
-			private set => SMC_PnlD.WriteArr(value.Panels);
-		}
-		public static int[] PanelA
-		{
-			get => SMC_PnlD?.ArrData;
-			private set => SMC_PnlD.WriteArr(value);
-		}
-
-		/// <summary>Sound配列情報</summary>
-		[Obsolete("SoundA(int型配列)を使用してください.")]
-		public static SoundD Sounds
-		{
-			get => new SoundD() { Sounds = SMC_SndD?.ArrData ?? new int[0] };
-			private set => SMC_SndD.WriteArr(value.Sounds);
-		}
-		public static int[] SoundA
-		{
-			get => SMC_SndD?.ArrData;
-			private set => SMC_SndD.WriteArr(value);
-		}
-
-		static public bool IsEnabled { get; private set; }
 
 		/// <summary>SharedMemoryを初期化する。</summary>
 		/// <param name="IsThisMother">書き込む側かどうか</param>
