3a5784db805b46a3c24eab1383942bcc3c4795be
ＴＲ <public@technotter.com>
2021-01-04 19:01:47 +0900

    Read/Writeメソッド群を分離

---
+++ BIDSSMemLib.rw/SMemLib.Read.cs
@@ -0,0 +1,78 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TR.BIDSSMemLib
+{
+	static public partial class SMemLib
+	{
+		/// <summary>共有メモリからデータを読み込む</summary>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static void Read()
+		{
+			if (NO_SMEM_MODE) return;
+			SMC_BSMD.OnlyRead();
+			SMC_OpenD.OnlyRead();
+			//Read<StaD>();
+			SMC_PnlD.OnlyRead();
+			SMC_SndD.OnlyRead();
+		}
+
+		/// <summary>共有メモリからデータを読み込む</summary>
+		/// <param name="D">読み込んだデータを書き込む変数</param>
+		/// <param name="DoWrite">ライブラリのデータを書き換えるかどうか</param>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static BIDSSharedMemoryData Read(out BIDSSharedMemoryData D, in bool DoWrite = true)
+		{
+			SMC_BSMD.Read(out D, DoWrite);
+			return D;
+		}
+		/// <summary>共有メモリからデータを読み込む</summary>
+		/// <param name="D">読み込んだデータを書き込む変数</param>
+		/// <param name="DoWrite">ライブラリのデータを書き換えるかどうか</param>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static OpenD Read(out OpenD D, in bool DoWrite = true)
+		{
+			SMC_OpenD.Read(out D, DoWrite);
+			return D;
+		}
+		/*
+		/// <summary>共有メモリからデータを読み込む</summary>
+		/// <param name="D">読み込んだデータを書き込む変数</param>
+		/// <param name="DoWrite">ライブラリのデータを書き換えるかどうか</param>
+		public StaD Read(out StaD D, bool DoWrite = true)
+		{
+			D = new StaD();
+			using (var m = MMFS?.CreateViewAccessor())
+			{
+				m?.Read(0, out D);
+			}
+			if (DoWrite) Stations = D;
+			return D;
+		}*/
+		/// <summary>共有メモリからデータを読み込む</summary>
+		/// <param name="D">読み込んだデータを書き込む変数</param>
+		/// <param name="DoWrite">ライブラリのデータを書き換えるかどうか</param>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static PanelD Read(out PanelD D, in bool DoWrite = true)
+		{
+			D = new PanelD() { Panels = SMC_PnlD.ReadArr(DoWrite) ?? new int[0] };
+			return D;
+		}
+		/// <summary>共有メモリからデータを読み込む</summary>
+		/// <param name="D">読み込んだデータを書き込む変数</param>
+		/// <param name="DoWrite">ライブラリのデータを書き換えるかどうか</param>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static SoundD Read(out SoundD D, in bool DoWrite = true)
+		{
+			D = new SoundD() { Sounds = SMC_SndD.ReadArr(DoWrite) ?? new int[0] };
+			return D;
+		}
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static BIDSSharedMemoryData ReadBSMD(in bool DoWrite = true) => SMC_BSMD.Read(DoWrite);
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static OpenD ReadOpenD(in bool DoWrite = true) => SMC_OpenD.Read(DoWrite);
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static int[] ReadPanel(in bool DoWrite = true) => SMC_PnlD.ReadArr(DoWrite);
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static int[] ReadSound(in bool DoWrite = true) => SMC_SndD.ReadArr(DoWrite);
+	}
+}
---
+++ BIDSSMemLib.rw/SMemLib.Write.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TR.BIDSSMemLib
+{
+	static public partial class SMemLib
+	{
+		/// <summary>BIDSSharedMemoryData構造体の情報を共有メモリに書き込む</summary>
+		/// <param name="D">書き込む構造体</param>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static void Write(in BIDSSharedMemoryData D) => SMC_BSMD?.Write(D);
+		/// <summary>OpenD構造体の情報を共有メモリに書き込む</summary>
+		/// <param name="D">書き込む構造体</param>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static void Write(in OpenD D) => SMC_OpenD?.Write(D);
+		/// <summary>StaD構造体の情報を共有メモリに書き込む</summary>
+		/// <param name="D">書き込む構造体</param>
+		//public void Write(in StaD D) => Write(D, 4);
+		/// <summary>Panel構造体の情報を共有メモリに書き込む</summary>
+		/// <param name="D">書き込む構造体</param>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static void Write(in PanelD D) => SMC_PnlD?.WriteArr(D.Panels);
+		/// <summary>Panel構造体の情報を共有メモリに書き込む</summary>
+		/// <param name="D">書き込む配列</param>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static void WritePanel(in int[] D) => SMC_PnlD?.WriteArr(D);
+		/// <summary>Sound構造体の情報を共有メモリに書き込む</summary>
+		/// <param name="D">書き込む構造体</param>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static void Write(in SoundD D) => SMC_SndD?.WriteArr(D.Sounds);
+		/// <summary>Sound構造体の情報を共有メモリに書き込む</summary>
+		/// <param name="D">書き込む配列</param>
+		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
+		public static void WriteSound(in int[] D) => SMC_SndD?.WriteArr(D);
+	}
+}
