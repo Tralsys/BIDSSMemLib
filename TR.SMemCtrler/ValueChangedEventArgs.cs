d36cd0798733ed022dd6184f5674252398a76ef0
ＴＲ <public@technotter.com>
2021-08-29 02:07:11 +0900

    ValueChangedEventArgsクラスを専用ファイルに分離

---
+++ TR.SMemCtrler/ValueChangedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TR
+{
+	/// <summary>値が変化した際に発火するイベントの引数</summary>
+	/// <typeparam name="T">値の型</typeparam>
+	public class ValueChangedEventArgs<T> : EventArgs
+	{
+		public readonly T OldValue;
+		public readonly T NewValue;
+		public ValueChangedEventArgs(in T oldValue, in T newValue)
+		{
+			OldValue = oldValue;
+			NewValue = newValue;
+		}
+	}
+}
