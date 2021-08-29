23dcc1da696356110354be90b9e03c78da2a3ae3
ＴＲ <public@technotter.com>
2021-08-29 19:41:36 +0900

    各メソッド/プロパティをインスタンスメソッド/プロパティ化

--- BIDSSMemLib.rw/SMemLib.Events.cs
+++ BIDSSMemLib.rw/SMemLib.Events.cs
@@ -12,27 +12,27 @@ namespace TR.BIDSSMemLib
 		[MethodImpl(SMemLib.MIOpt)]//関数のインライン展開を積極的にやってもらう.
 		static internal double MtoKM(in float m) => m / 1000;
 	}
-	public static partial class SMemLib
+
+	public partial class SMemLib
 	{
-	//イベントクラスとイベントを列挙
+		//イベントクラスとイベントを列挙
 
-
-		public static event EventHandler<ValueChangedEventArgs<BIDSSharedMemoryData>> SMC_BSMDChanged
+		public event EventHandler<ValueChangedEventArgs<BIDSSharedMemoryData>> SMC_BSMDChanged
 		{
 			add => SMC_BSMD.ValueChanged += value;
 			remove => SMC_BSMD.ValueChanged -= value;
 		}
-		public static event EventHandler<ValueChangedEventArgs<OpenD>> SMC_OpenDChanged
+		public event EventHandler<ValueChangedEventArgs<OpenD>> SMC_OpenDChanged
 		{
 			add => SMC_OpenD.ValueChanged += value;
 			remove => SMC_OpenD.ValueChanged -= value;
 		}
-		public static event EventHandler<ValueChangedEventArgs<int[]>> SMC_PanelDChanged
+		public event EventHandler<ValueChangedEventArgs<int[]>> SMC_PanelDChanged
 		{
 			add => SMC_PnlD.ArrValueChanged += value;
 			remove => SMC_PnlD.ArrValueChanged -= value;
 		}
-		public static event EventHandler<ValueChangedEventArgs<int[]>> SMC_SoundDChanged
+		public event EventHandler<ValueChangedEventArgs<int[]>> SMC_SoundDChanged
 		{
 			add => SMC_SndD.ArrValueChanged += value;
 			remove => SMC_SndD.ArrValueChanged -= value;
--- BIDSSMemLib.rw/SMemLib.Read.cs
+++ BIDSSMemLib.rw/SMemLib.Read.cs
@@ -2,15 +2,12 @@
 
 namespace TR.BIDSSMemLib
 {
-	static public partial class SMemLib
+	public partial class SMemLib
 	{
 		/// <summary>共有メモリからデータを読み込む</summary>
 		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
-		public static void Read()
+		public void Read()
 		{
-			if (NO_SMEM_MODE)
-				return;
-
 			_ = SMC_BSMD.Read();
 			_ = SMC_OpenD.Read();
 			_ = SMC_PnlD.Read();
@@ -21,19 +18,19 @@ namespace TR.BIDSSMemLib
 		/// <param name="D">読み込んだデータを書き込む変数</param>
 		/// <param name="DoWrite">ライブラリのデータを書き換えるかどうか</param>
 		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
-		public static BIDSSharedMemoryData Read(out BIDSSharedMemoryData D) => D = SMC_BSMD.Read();
+		public BIDSSharedMemoryData Read(out BIDSSharedMemoryData D) => D = SMC_BSMD.Read();
 
 		/// <summary>共有メモリからデータを読み込む</summary>
 		/// <param name="D">読み込んだデータを書き込む変数</param>
 		/// <param name="DoWrite">ライブラリのデータを書き換えるかどうか</param>
 		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
-		public static OpenD Read(out OpenD D) => D = SMC_OpenD.Read();
+		public OpenD Read(out OpenD D) => D = SMC_OpenD.Read();
 
 		/// <summary>共有メモリからデータを読み込む</summary>
 		/// <param name="D">読み込んだデータを書き込む変数</param>
 		/// <param name="DoWrite">ライブラリのデータを書き換えるかどうか</param>
 		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
-		public static PanelD Read(out PanelD D)
+		public PanelD Read(out PanelD D)
 		{
 			D = new PanelD() { Panels = SMC_PnlD.Read().ToArray() ?? new int[0] };
 			return D;
@@ -42,18 +39,18 @@ namespace TR.BIDSSMemLib
 		/// <param name="D">読み込んだデータを書き込む変数</param>
 		/// <param name="DoWrite">ライブラリのデータを書き換えるかどうか</param>
 		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
-		public static SoundD Read(out SoundD D)
+		public SoundD Read(out SoundD D)
 		{
 			D = new SoundD() { Sounds = SMC_SndD.Read().ToArray() ?? new int[0] };
 			return D;
 		}
 		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
-		public static BIDSSharedMemoryData ReadBSMD() => SMC_BSMD.Read();
+		public BIDSSharedMemoryData ReadBSMD() => SMC_BSMD.Read();
 		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
-		public static OpenD ReadOpenD() => SMC_OpenD.Read();
+		public OpenD ReadOpenD() => SMC_OpenD.Read();
 		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
-		public static int[] ReadPanel() => SMC_PnlD.Read().ToArray();
+		public int[] ReadPanel() => SMC_PnlD.Read().ToArray();
 		[MethodImpl(MIOpt)]//関数のインライン展開を積極的にやってもらう.
-		public static int[] ReadSound() => SMC_SndD.Read().ToArray();
+		public int[] ReadSound() => SMC_SndD.Read().ToArray();
 	}
 }
