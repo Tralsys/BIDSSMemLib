b2581e17be32262b501c122ade45bd3ff9f4147e
ＴＲ <public@technotter.com>
2021-08-31 02:28:53 +0900

    base.ReadArrayとbase.WriteArrayが逆だったので修正
    双方とも中身は一緒なので, コード上のミスだけだった

--- TR.SMemIF/SemaphorelessSMemIF.net20.cs
+++ TR.SMemIF/SemaphorelessSMemIF.net20.cs
@@ -80,7 +80,7 @@ namespace TR
 		/// <returns>読み取りに成功したかどうか</returns>
 		public override bool ReadArray<T>(long pos, T[] buf, int offset, int count) where T : struct
 		{
-			if (!base.WriteArray(pos, buf, offset, count) || MMVA == IntPtr.Zero)
+			if (!base.ReadArray(pos, buf, offset, count) || MMVA == IntPtr.Zero)
 				return false;
 
 			//読み取り開始位置適用済みのポインタを取得する
@@ -157,7 +157,7 @@ namespace TR
 		/// <returns>書き込みに成功したかどうか</returns>
 		public override bool WriteArray<T>(long pos, T[] buf, int offset, int count) where T : struct
 		{
-			if (!base.ReadArray(pos, buf, offset, count) || MMVA == IntPtr.Zero)
+			if (!base.WriteArray(pos, buf, offset, count) || MMVA == IntPtr.Zero)
 				return false;
 
 			IntPtr ip_writeTo = new IntPtr(MMVA.ToInt64() + pos);//読み取り開始位置適用済みのポインタ
