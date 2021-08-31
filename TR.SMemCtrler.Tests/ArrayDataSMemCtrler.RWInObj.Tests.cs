4120b60579abd79c06ab2d643f704a8d1956b903
ＴＲ <public@technotter.com>
2021-08-31 21:12:10 +0900

    大量のデータの読み書きテストをNET35で行わないように修正

--- TR.SMemCtrler.Tests/ArrayDataSMemCtrler.RWInObj.Tests.cs
+++ TR.SMemCtrler.Tests/ArrayDataSMemCtrler.RWInObj.Tests.cs
@@ -10,6 +10,7 @@ namespace TR
 		const int ARRLEN_MAX = 0x400;
 		const int ARRLEN_DOUBLE_MAX = 0x200;
 		const int ARRLEN_CUSTOMSTRUCT_MAX = 0xA9;
+		const int ARRLEN_MANY_CUSTOMSTRUCT_MAX = 0x400;
 		const int TESTCASE_COUNT = 10;
 		static Random random = new();
 		static int rand_int => random.Next();
@@ -58,6 +59,22 @@ namespace TR
 				D = (rand_int % 2) == 0
 			});
 		}
+
+#if NET5_0_OR_GREATER || NETCOREAPP
+		[Test, Parallelizable]
+		public void ManyCustomStructListRWTest([Random(1, ARRLEN_MANY_CUSTOMSTRUCT_MAX, TESTCASE_COUNT)] int count)
+		{
+			string smem_name = $"{nameof(ManyCustomStructListRWTest)}_{count}";
+
+			RunTest(smem_name, count, _ => new CustomStruct()
+			{
+				A = (float)rand_double,
+				B = rand_int,
+				C = rand_double,
+				D = (rand_int % 2) == 0
+			});
+		}
+#endif
 
 		private static void RunTest<T>(string smem_name, int count, Func<int, T> generator) where T : struct
 		{
