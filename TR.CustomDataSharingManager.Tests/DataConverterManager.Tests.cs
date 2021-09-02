a5aad9a5ef32a2297bfc83070df7824ff2bf3969
ＴＲ <public@technotter.com>
2021-09-02 14:37:31 +0900

    DataConverterManagerのテスト実装を追加

---
+++ TR.CustomDataSharingManager.Tests/DataConverterManager.Tests.cs
@@ -0,0 +1,58 @@
+using NUnit.Framework;
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace TR
+{
+	public class DataConverterManagerTests
+	{
+
+		DataForConverter CreateEmptyDataForConverter(CustomDataSharingManager cdsManager) => new(cdsManager, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default);
+
+		[Test]
+		public Task LoadScriptsFromAssemblyTest()
+		{
+			using DataConverterManager manager = new();
+			using CustomDataSharingManager cdsManager = new();
+			manager.LoadScriptsFromAssembly(Assembly.GetExecutingAssembly());
+
+			var testArg = CreateEmptyDataForConverter(cdsManager);
+
+			//‚±‚±‚ÅAssert.Pass‚ª“ü‚é
+			return manager.RunAsync(testArg);
+		}
+
+		const int NumberInTestScript = 12345;
+		static readonly string TestScript = $"pPanel[0] = {NumberInTestScript};";
+		[Test]
+		public async Task CreateActionFromScriptStringTest()
+		{
+			Func<DataForConverter, Task> func =  DataConverterManager.CreateActionFromScriptString(TestScript);
+
+			IntPtr panelPtr = Marshal.AllocHGlobal(sizeof(int));
+
+			using CustomDataSharingManager cdsManager = new();
+
+			DataForConverter data = CreateEmptyDataForConverter(cdsManager) with { Panel = panelPtr };
+
+			await func.Invoke(data);
+
+			int result = Marshal.ReadInt32(panelPtr);
+
+			Assert.AreEqual(NumberInTestScript, result);
+		}
+
+	}
+
+	public class TestingWithIScriptingModule : IScriptingModule
+	{
+		public async Task RunAsync(DataForConverter data)
+		{
+			await Task.Delay(1);
+			Assert.Pass();
+		}
+	}
+}
