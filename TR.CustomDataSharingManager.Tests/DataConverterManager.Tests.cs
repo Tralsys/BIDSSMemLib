9288a42985441fe18cc21e58a2741f927106d973
ＴＲ <public@technotter.com>
2021-09-03 23:22:28 +0900

    Update DataConverterManager.Tests.cs
    - DataForConverterのPanel/Soundに対してインデクサによるアクセスを実装したことに対応
    - FreeHGlobalを実装し忘れていたので追加
    - LoadScriptsFromAssemblyTestで値の書き換えによるテストを行うように修正

--- TR.CustomDataSharingManager.Tests/DataConverterManager.Tests.cs
+++ TR.CustomDataSharingManager.Tests/DataConverterManager.Tests.cs
@@ -10,23 +10,30 @@ namespace TR
 	public class DataConverterManagerTests
 	{
 
-		DataForConverter CreateEmptyDataForConverter(CustomDataSharingManager cdsManager) => new(cdsManager, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default);
+		DataForConverter CreateEmptyDataForConverter(CustomDataSharingManager cdsManager) => new(cdsManager, new(IntPtr.Zero), new(IntPtr.Zero), default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default, default);
 
 		[Test]
-		public Task LoadScriptsFromAssemblyTest()
+		public async Task LoadScriptsFromAssemblyTest()
 		{
 			using DataConverterManager manager = new();
 			using CustomDataSharingManager cdsManager = new();
+			IntPtr panelPtr = Marshal.AllocHGlobal(sizeof(int));
+
 			manager.LoadScriptsFromAssembly(Assembly.GetExecutingAssembly());
 
-			var testArg = CreateEmptyDataForConverter(cdsManager);
+			var testArg = CreateEmptyDataForConverter(cdsManager) with { Panel = new(panelPtr) };
 
 			//‚±‚±‚ÅAssert.Pass‚ª“ü‚é
-			return manager.RunAsync(testArg);
+			await manager.RunAsync(testArg);
+
+			Assert.AreEqual(TestingWithIScriptingModule.TestData, testArg.Panel[0]);
+			Assert.AreEqual(TestingWithIScriptingModule.TestData, Marshal.ReadInt32(panelPtr));
+
+			Marshal.FreeHGlobal(panelPtr);
 		}
 
 		const int NumberInTestScript = 12345;
-		static readonly string TestScript = $"unsafe {{ Panel[0] = {NumberInTestScript}; }}";
+		static readonly string TestScript = $"Panel[0] = {NumberInTestScript};";
 		[Test]
 		public async Task CreateActionFromScriptStringTest()
 		{
@@ -36,23 +43,26 @@ namespace TR
 
 			using CustomDataSharingManager cdsManager = new();
 
-			DataForConverter data = CreateEmptyDataForConverter(cdsManager) with { PanelIntPtr = panelPtr };
+			DataForConverter data = CreateEmptyDataForConverter(cdsManager) with { Panel = new(panelPtr) };
 
 			await func.Invoke(data);
 
 			int result = Marshal.ReadInt32(panelPtr);
 
 			Assert.AreEqual(NumberInTestScript, result);
-		}
 
+			Marshal.FreeHGlobal(panelPtr);
+		}
 	}
 
 	public class TestingWithIScriptingModule : IScriptingModule
 	{
+		public const int TestData = 12345;
 		public async Task RunAsync(DataForConverter data)
 		{
 			await Task.Delay(1);
-			Assert.Pass();
+
+			data.Panel[0] = TestData;
 		}
 	}
 }
