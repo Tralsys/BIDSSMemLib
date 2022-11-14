91968f28af85b163cab5807dbd3fa5c30d8ed079
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-11-14 23:45:40 +0900

    Create MemberInfoToVariableDataRecordList.cs

---
+++ BIDSSMemLib.Variable/Utils/MemberInfoToVariableDataRecordList.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using BIDS.Parser.Variable;
+
+namespace TR.BIDSSMemLib;
+
+public static partial class Utils
+{
+	public static List<VariableStructure.IDataRecord> ToVariableDataRecordList(this Type type)
+		=> type
+			.GetMembers(BindingFlags.Public | BindingFlags.Instance)
+			.Where(v => v.MemberType is MemberTypes.Field or MemberTypes.Property)
+			.Select(v => v.ToVariableDataRecord())
+			.ToList();
+}
