cee99a06725597d9bb10887e8349cb82e0cd30b4
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-11-14 23:45:32 +0900

    Create TypeToVariableDataRecord.cs

---
+++ BIDSSMemLib.Variable/Utils/TypeToVariableDataRecord.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Reflection;
+
+using BIDS.Parser.Variable;
+
+using TR.BIDSSMemLib.Variable;
+
+namespace TR.BIDSSMemLib;
+
+public static partial class Utils
+{
+	public static VariableStructure.IDataRecord ToVariableDataRecord(this MemberInfo member)
+		=> ToVariableDataRecord(member switch
+		{
+			FieldInfo v => v.FieldType,
+			PropertyInfo v => v.PropertyType,
+
+			_ => throw new ArgumentException("Only Field or Property is supported"),
+		}, member.Name);
+
+	public static VariableStructure.IDataRecord ToVariableDataRecord(Type memberType, string name)
+	{
+		if (memberType.IsArray)
+		{
+			if (memberType.GetElementType() is not Type elemType)
+				throw new Exception("Cannot recognize Array element type");
+
+			VariableDataType elemDataType = elemType.ToVariableDataType();
+			if (elemDataType == VariableDataType.Array)
+				throw new NotSupportedException("Currently, nested array is not supported.");
+
+			return new VariableStructure.ArrayStructure(elemDataType, name);
+		}
+
+		if (memberType == typeof(string))
+		{
+			return new VariableStructure.ArrayStructure(VariableDataType.UInt8, name);
+		}
+
+		return new VariableStructure.DataRecord(memberType.ToVariableDataType(), name);
+	}
+}
