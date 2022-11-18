b4f0a2904a5373b12cf13a52dc6a080948b6cb58
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-11-18 15:42:31 +0900

    Add tests for `TypeToVariableDataType`

--- BIDSSMemLib.Variable.Tests/UtilsTests/TypeToVariableDataTypeTests.cs
+++ BIDSSMemLib.Variable.Tests/UtilsTests/TypeToVariableDataTypeTests.cs
@@ -1,5 +1,37 @@
+using BIDS.Parser.Variable;
+
+using TR.BIDSSMemLib.Variable;
+
 namespace BIDSSMemLib.Variable.Tests.UtilsTests;
 
 public class TypeToVariableDataTypeTests
 {
+	[TestCase(typeof(bool), VariableDataType.Boolean)]
+	[TestCase(typeof(sbyte), VariableDataType.Int8)]
+	[TestCase(typeof(short), VariableDataType.Int16)]
+	[TestCase(typeof(int), VariableDataType.Int32)]
+	[TestCase(typeof(long), VariableDataType.Int64)]
+	[TestCase(typeof(byte), VariableDataType.UInt8)]
+	[TestCase(typeof(ushort), VariableDataType.UInt16)]
+	[TestCase(typeof(uint), VariableDataType.UInt32)]
+	[TestCase(typeof(ulong), VariableDataType.UInt64)]
+	[TestCase(typeof(float), VariableDataType.Float32)]
+	[TestCase(typeof(double), VariableDataType.Float64)]
+	[TestCase(typeof(Half), VariableDataType.Float16)]
+	[TestCase(typeof(string), VariableDataType.Array)]
+	[TestCase(typeof(object[]), VariableDataType.Array)]
+	public void NormalCaseTests(Type type, VariableDataType expectedVariableDataType)
+	{
+		Assert.That(type.ToVariableDataType(), Is.EqualTo(expectedVariableDataType));
+	}
+
+	struct SampleStruct { }
+
+	[TestCase(typeof(SampleStruct))]
+	[TestCase(typeof(object))]
+	[TestCase(typeof(Array))]
+	public void ExceptionTests(Type type)
+	{
+		Assert.Throws(typeof(NotSupportedException), () => type.ToVariableDataType());
+	}
 }
