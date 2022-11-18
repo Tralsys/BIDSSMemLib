07cda02eef5beef1b8b6417fd7a7a3b25f31f100
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-11-18 16:36:31 +0900

    Add tests for `MemberInfoToVariableDataRecordList`

--- BIDSSMemLib.Variable.Tests/UtilsTests/MemberInfoToVariableDataRecordListTests.cs
+++ BIDSSMemLib.Variable.Tests/UtilsTests/MemberInfoToVariableDataRecordListTests.cs
@@ -1,5 +1,52 @@
+using BIDS.Parser.Variable;
+
+using TR.BIDSSMemLib.Variable;
+
 namespace BIDSSMemLib.Variable.Tests.UtilsTests;
 
 public class MemberInfoToVariableDataRecordListTests
 {
+	class SampleBaseClass
+	{
+		public int SampleBaseIntProperty { get; }
+		public long SampleBaseLongField;
+
+		private int PrivateBaseIntProperty { get; }
+		protected string ProtectedBaseStringField = string.Empty;
+		internal char InternalBaseCharProperty { get; }
+
+		public event EventHandler? BasePublicEvent;
+		private event EventHandler? BasePrivateEvent;
+
+		public void BasePublicMethod() => BasePrivateEvent?.Invoke(this, new());
+		private void BasePrivateMethod() => BasePrivateEvent?.Invoke(this, new());
+
+		static public byte PublicBaseStaticCharProperty { get; }
+	}
+
+	class SampleClass : SampleBaseClass
+	{
+		public int SampleIntProperty { get; }
+		public long[]? SampleLongArrayField;
+
+		private int PrivateIntProperty { get; }
+		protected string ProtectedStringField = string.Empty;
+		internal char InternalCharProperty { get; }
+	}
+
+	[Test]
+	public void NormalTest()
+	{
+		Assert.That(
+			typeof(SampleClass).ToVariableDataRecordList(),
+			Is.EquivalentTo(new List<VariableStructure.IDataRecord>()
+			{
+				new VariableStructure.DataRecord(VariableDataType.Int32, nameof(SampleClass.SampleIntProperty)),
+				new VariableStructure.ArrayStructure(VariableDataType.Int64, nameof(SampleClass.SampleLongArrayField)),
+
+				new VariableStructure.DataRecord(VariableDataType.Int32, nameof(SampleBaseClass.SampleBaseIntProperty)),
+				new VariableStructure.DataRecord(VariableDataType.Int64, nameof(SampleBaseClass.SampleBaseLongField)),
+			})
+		);
+	}
 }
