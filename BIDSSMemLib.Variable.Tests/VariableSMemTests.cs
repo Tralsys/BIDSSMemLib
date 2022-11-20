d2e3592ff8c603e5cbac1f2b79758198d1fd260e
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-11-20 21:38:31 +0900

    add test for `ReadFromSMem`

--- BIDSSMemLib.Variable.Tests/VariableSMemTests.cs
+++ BIDSSMemLib.Variable.Tests/VariableSMemTests.cs
@@ -186,4 +186,96 @@ public partial class VariableSMemTests
 			Is.EquivalentTo(expectedBytes)
 		);
 	}
+
+	[Test]
+	public void ReadFromSMemTest_ReturnPayload()
+	{
+		SMemIFMock SMemIF = new("test", SMemCapacity);
+
+		VariableSMem<SampleClass> variableSMem = new(SMemIF);
+		VariableSMem<SampleClass> variableSMem1 = new(new SMemIFMock(SMemIF));
+
+		SampleClass SampleData = new()
+		{
+			vUInt16 = 2,
+			vInt32 = -2,
+			vInt64 = 0x7FFFFFFF12,
+			vFloat64 = 1.23456789,
+			vString = "testString😀",
+			vInt32Arr = new int[]
+			{
+				1,
+				2,
+				3
+			}
+		};
+
+		variableSMem.WriteToSMem(SampleData);
+
+		VariableStructurePayload payload = variableSMem1.ReadFromSMem();
+
+		Assert.That(payload, Has.Count.EqualTo(6));
+
+		Assert.That(payload, Has.Member(new KeyValuePair<string, VariableStructure.IDataRecord>(
+			nameof(SampleClass.vUInt16),
+			SampleClass.Expected_vUInt16 with
+			{
+				Value = SampleData.vUInt16,
+			})
+		));
+		Assert.That(payload, Has.Member(new KeyValuePair<string, VariableStructure.IDataRecord>(
+			nameof(SampleClass.vInt32),
+			SampleClass.Expected_vInt32 with
+			{
+				Value = SampleData.vInt32,
+			})
+		));
+		Assert.That(payload, Has.Member(new KeyValuePair<string, VariableStructure.IDataRecord>(
+			nameof(SampleClass.vInt64),
+			SampleClass.Expected_vInt64 with
+			{
+				Value = SampleData.vInt64,
+			})
+		));
+		Assert.That(payload, Has.Member(new KeyValuePair<string, VariableStructure.IDataRecord>(
+			nameof(SampleClass.vFloat64),
+			SampleClass.Expected_vFloat64 with
+			{
+				Value = SampleData.vFloat64,
+			})
+		));
+
+		ArrayStructureInPayloadTestUtil(payload, nameof(SampleClass.vString), SampleClass.Expected_vString, new byte[]
+		{
+			0x74,
+			0x65,
+			0x73,
+			0x74,
+			0x53,
+			0x74,
+			0x72,
+			0x69,
+			0x6e,
+			0x67,
+			0xf0,
+			0x9f,
+			0x98,
+			0x80,
+		});
+
+		ArrayStructureInPayloadTestUtil(payload, nameof(SampleClass.vInt32Arr), SampleClass.Expected_vInt32Arr, SampleData.vInt32Arr);
+	}
+
+	static void ArrayStructureInPayloadTestUtil(VariableStructurePayload payload, string name, VariableStructure.ArrayStructure expectedStructure, Array expectedArray)
+	{
+		Assert.That(payload, Does.ContainKey(name));
+		VariableStructure.ArrayStructure? actual = payload[name] as VariableStructure.ArrayStructure;
+
+		Assert.That(actual, Is.EqualTo(expectedStructure with
+		{
+			ValueArray = actual!.ValueArray
+		}));
+
+		Assert.That(actual.ValueArray, Is.EquivalentTo(expectedArray));
+	}
 }
