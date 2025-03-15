391e4947739565ad3b2c4ea54e09b0a9530153ce
TetsuOtter <31824852+TetsuOtter@users.noreply.github.com>
2025-03-16 02:39:21 +0900

    BIDS-RTC実装

--- BIDSSMemLib.BveEX/BveExInterface.cs
+++ BIDSSMemLib.BveEX/BveExInterface.cs
@@ -1,20 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 using BveEx.PluginHost.Plugins;
 using BveEx.PluginHost.Plugins.Extensions;
 
 using BveTypes.ClassWrappers;
 
+using SIPSorcery.Net;
+
+using TR.BidsWebRtc.Api;
+using TR.BidsWebRtc.WebRtc;
+
 namespace TR.BIDSSMemLib;
 
 [Plugin(PluginType.Extension)]
 public partial class BveExInterface : AssemblyPluginBase, IExtension
 {
+	static readonly HttpClient httpClient = new();
 	readonly SMemLib smemLib = new(
 		isNoSMemMode: false,
 		isNoEventMode: true,
 		isNoOptionalEventMode: true
 	);
+
+	readonly RtcConnectionManager? rtcConnection = null;
+	readonly HashSet<RTCDataChannel> rtcBsmdSendList = [];
+	readonly HashSet<RTCDataChannel> rtcPanelSendList = [];
+	readonly HashSet<RTCDataChannel> rtcSoundSendList = [];
 
 	public BveExInterface(PluginBuilder builder) : base(builder)
 	{
@@ -23,7 +39,119 @@ public partial class BveExInterface : AssemblyPluginBase, IExtension
 		smemLib.WritePanel(new int[panelArrayLength]);
 		smemLib.WriteSound(new int[soundArrayLength]);
 
+		rtcConnection = CreateRtcConnectionManager();
+		if (rtcConnection is not null)
+		{
+			rtcConnection.OnDataChannelClosed += RtcConnection_OnDataChannelClosed;
+			rtcConnection.OnDataGot += RtcConnection_OnDataGot;
+		}
+
 		BveHacker.ScenarioClosed += OnScenarioClosed;
+	}
+
+	private static RtcConnectionManager? CreateRtcConnectionManager()
+	{
+		Assembly assembly = Assembly.GetExecutingAssembly();
+		if (assembly is null)
+		{
+			return null;
+		}
+
+		string assemblyLocation = assembly.Location;
+		string asmDirectory = Path.GetDirectoryName(assemblyLocation);
+		string tokenFilePath = Path.Combine(asmDirectory, "bids-rtc.token.bin");
+		if (!File.Exists(tokenFilePath))
+		{
+			return null;
+		}
+
+		byte[] tokenBytes = File.ReadAllBytes(tokenFilePath);
+		TokenManager tokenManager = new(httpClient, tokenBytes);
+		SdpExchangeApi sdpExchangeApi = new(httpClient, tokenManager);
+		RtcConnectionManager rtcConnection = RtcConnectionManager.Create(RtcConnectionManager.Role.Provider, sdpExchangeApi);
+
+		return rtcConnection;
+	}
+
+	private void RtcConnection_OnDataGot(object sender, RtcConnectionManager.OnDataGotEventArgs e)
+	{
+		if (e.DataChannel is null || e.Data.Length <= 2)
+		{
+			return;
+		}
+		if (e.Data[0] != (byte)'{' || e.Data[^1] != (byte)'}')
+		{
+			return;
+		}
+
+		try
+		{
+			string json = System.Text.Encoding.UTF8.GetString(e.Data);
+			var jsonData = System.Text.Json.JsonDocument.Parse(json).RootElement;
+			if (!jsonData.TryGetProperty("id", out var idField) || !jsonData.TryGetProperty("cmd", out var cmdField))
+			{
+				return;
+			}
+			string? id = idField.GetString();
+			string? cmd = cmdField.GetString();
+			if (id is null || cmd is null)
+			{
+				return;
+			}
+
+			bool isSuccess = false;
+			switch (cmd)
+			{
+				case "BEGIN_BSMD":
+					rtcBsmdSendList.Add(e.DataChannel);
+					isSuccess = true;
+					break;
+				case "END_BSMD":
+					rtcBsmdSendList.Remove(e.DataChannel);
+					isSuccess = true;
+					break;
+				case "BEGIN_PANEL":
+					rtcPanelSendList.Add(e.DataChannel);
+					isSuccess = true;
+					break;
+				case "END_PANEL":
+					rtcPanelSendList.Remove(e.DataChannel);
+					isSuccess = true;
+					break;
+				case "BEGIN_SOUND":
+					rtcSoundSendList.Add(e.DataChannel);
+					isSuccess = true;
+					break;
+				case "END_SOUND":
+					rtcSoundSendList.Remove(e.DataChannel);
+					isSuccess = true;
+					break;
+				// TODO: WATCH / UNWATCH対応
+			}
+
+			//string response = $$"""{"id": "{{id}}"}""";
+			using var responseBytes = new MemoryStream();
+			using var response = new System.Text.Json.Utf8JsonWriter(responseBytes);
+			response.WriteStartObject();
+			response.WriteString("id", id);
+			response.WriteBoolean("success", isSuccess);
+			response.WriteEndObject();
+			e.DataChannel.send(responseBytes.ToArray());
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"error from {e.ClientId}: {ex}");
+		}
+	}
+	private void RtcConnection_OnDataChannelClosed(object sender, RtcConnectionManager.OnDataChannelStateChangedEventArgs e)
+	{
+		if (e.DataChannel is null)
+		{
+			return;
+		}
+		rtcBsmdSendList.Remove(e.DataChannel);
+		rtcPanelSendList.Remove(e.DataChannel);
+		rtcSoundSendList.Remove(e.DataChannel);
 	}
 
 	nuint panelArrayLength = 256;
@@ -35,6 +163,14 @@ public partial class BveExInterface : AssemblyPluginBase, IExtension
 		smemLib.WritePanel(new int[panelArrayLength]);
 		smemLib.WriteSound(new int[soundArrayLength]);
 		smemLib.Dispose();
+
+		rtcConnection?.Dispose();
+
+		if (bsmdPtr != IntPtr.Zero)
+		{
+			Marshal.FreeHGlobal(bsmdPtr);
+			bsmdPtr = IntPtr.Zero;
+		}
 
 		BveHacker.ScenarioClosed -= OnScenarioClosed;
 	}
@@ -49,7 +185,7 @@ public partial class BveExInterface : AssemblyPluginBase, IExtension
 	readonly bool[] keyStateArray = new bool[CtrlInput.KeyArrSizeMax];
 	BveInstanceManager? bveInstanceManager = null;
 
-  public override void Tick(TimeSpan elapsed)
+	public override void Tick(TimeSpan elapsed)
 	{
 		if (BveHacker.IsScenarioCreated)
 		{
@@ -57,6 +193,7 @@ public partial class BveExInterface : AssemblyPluginBase, IExtension
 
 			bveInstanceManager.setBIDSSharedMemoryData(ref bsmd);
 			smemLib.Write(bsmd);
+			sendRtcBsmd(in bsmd);
 
 			bveInstanceManager.setOpenD(ref openD, in elapsed);
 			smemLib.Write(openD);
@@ -64,10 +201,12 @@ public partial class BveExInterface : AssemblyPluginBase, IExtension
 			int[] panelArray = bveInstanceManager.atsPlugin.PanelArray;
 			panelArrayLength = (nuint)panelArray.Length;
 			smemLib.WritePanel(panelArray);
+			sendRtcPanel(panelArray);
 
 			int[] soundArray = bveInstanceManager.atsPlugin.SoundArray;
 			soundArrayLength = (nuint)soundArray.Length;
 			smemLib.WriteSound(soundArray);
+			sendRtcSound(soundArray);
 
 			// BVEへの入力処理
 			Hands currentHands = CtrlInput.GetHandD();
@@ -139,11 +278,100 @@ public partial class BveExInterface : AssemblyPluginBase, IExtension
 		};
 		openD = new();
 		smemLib.Write(bsmd);
+		sendRtcBsmd(in bsmd);
 		smemLib.Write(openD);
-		smemLib.WritePanel(new int[panelArrayLength]);
-		smemLib.WriteSound(new int[soundArrayLength]);
+		int[] panelArray = new int[panelArrayLength];
+		smemLib.WritePanel(panelArray);
+		sendRtcPanel(panelArray);
+		int[] soundArray = panelArrayLength == soundArrayLength ? panelArray : new int[soundArrayLength];
+		smemLib.WriteSound(soundArray);
+		sendRtcSound(soundArray);
 
 		bveInstanceManager = null;
+	}
+
+	static readonly int specSize = Marshal.SizeOf<Spec>();
+	static readonly int stateSize = Marshal.SizeOf<State>();
+	static readonly int handSize = Marshal.SizeOf<Hand>();
+	static readonly int bsmdSize = 1 + 4 + specSize + stateSize + handSize + 1;
+	static readonly int specOffset = 1 + 4;
+	static readonly int stateOffset = specOffset + specSize;
+	static readonly int handleOffset = stateOffset + stateSize;
+	static readonly int doorClosedOffset = handleOffset + handSize;
+	readonly byte[] bsmdBytes = new byte[bsmdSize];
+	IntPtr bsmdPtr = IntPtr.Zero;
+	void sendRtcBsmd(in BIDSSharedMemoryData bsmd)
+	{
+		if (rtcBsmdSendList.Count == 0)
+		{
+			return;
+		}
+		if (bsmdBytes[0] == 0)
+		{
+			bsmdBytes[0] = 0x74;
+			bsmdBytes[1] = 0x72;
+			bsmdBytes[2] = 0x57;
+			bsmdBytes[3] = 0x42;
+		}
+		if (bsmdPtr == IntPtr.Zero)
+		{
+			bsmdPtr = Marshal.AllocHGlobal(bsmdSize);
+		}
+
+		bsmdBytes[4] = bsmd.IsEnabled ? (byte)1 : (byte)0;
+		BitConverter.GetBytes(bsmd.VersionNum).CopyTo(bsmdBytes, 4 + 1);
+
+		Marshal.StructureToPtr(bsmd.SpecData, bsmdPtr, false);
+		Marshal.Copy(bsmdPtr, bsmdBytes, 4 + specOffset, specSize);
+
+		Marshal.StructureToPtr(bsmd.StateData, bsmdPtr, false);
+		Marshal.Copy(bsmdPtr, bsmdBytes, 4 + stateOffset, stateSize);
+
+		Marshal.StructureToPtr(bsmd.HandleData, bsmdPtr, false);
+		Marshal.Copy(bsmdPtr, bsmdBytes, 4 + handleOffset, handSize);
+
+		bsmdBytes[4 + doorClosedOffset] = bsmd.IsDoorClosed ? (byte)1 : (byte)0;
+
+		foreach (RTCDataChannel rtcDataChannel in rtcBsmdSendList)
+		{
+			try
+			{
+				rtcDataChannel.send(bsmdBytes);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"error: {ex}");
+			}
+		}
+	}
+	void sendRtcPanel(in int[] panelArray)
+		=> sendRtcArray(panelArray, rtcPanelSendList, 0x50);
+	void sendRtcSound(in int[] panelArray)
+		=> sendRtcArray(panelArray, rtcPanelSendList, 0x53);
+	static void sendRtcArray(in int[] srcArray, in HashSet<RTCDataChannel> rtcDataChannelList, byte type)
+	{
+		if (rtcDataChannelList.Count == 0)
+		{
+			return;
+		}
+		byte[] bytes = new byte[4 + 4 + srcArray.Length * sizeof(int)];
+		bytes[0] = 0x74;
+		bytes[1] = 0x72;
+		bytes[2] = 0x57;
+		bytes[3] = type;
+		BitConverter.GetBytes(srcArray.Length).CopyTo(bytes, 4);
+		Buffer.BlockCopy(srcArray, 0, bytes, 4 + 4, srcArray.Length * sizeof(int));
+		foreach (RTCDataChannel rtcDataChannel in rtcDataChannelList)
+		{
+			try
+			{
+				rtcDataChannel.send(bytes);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"error: {ex}");
+			}
+		}
 	}
 
 	class BveInstanceManager
